73a087682d0fe6ff090d6ee1fc72cdf33dd460f3
PetraJM <114162153+Petramanullang@users.noreply.github.com>
2024-09-24 16:44:10 +0700

    tugas python 2

---
+++ No-1.py
@@ -0,0 +1,5 @@
+# Cara Konfersi Rupiah ke Dollar
+
+Rupiah = int(input("Masukkan Rupiah: Rp."))
+USD = Rupiah /  15236
+print(f"{USD:.2f} USD")
---
+++ No-2.py
@@ -0,0 +1,7 @@
+# Cara Mencari Daya Listrik
+
+Tegangan = int(input("Masukkan Tegangan (Volt): "))
+Arus = int(input("Masukkan Arus (ampere): "))
+
+Daya = Tegangan * Arus
+print (f"Daya = {Daya} Watt")
